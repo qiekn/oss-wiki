a297694836abd4bffc628b945f892997788b6a07
qiekn <qiekndev@gmail.com>
2026-06-19 23:28:29 +0800

    CI: deploy Next.js app to Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+// https://qiekn.github.io/oss-wiki/logo.png  (right √)
+// https://qiekn.github.io/logo.png           (wrong ×)
+const repositoryName = process.env.REPOSITORY_NAME || "";
+const isDev = process.env.NODE_ENV === "development";
+const basePath = isDev || !repositoryName ? "" : `/${repositoryName}`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Static build, because we use github pages
+  output: "export",
+
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : undefined,
+
+  // We use github pages, so no server side optimize
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
